b16bfa5124bce2b6973af63a1f9d0319f33b21cf
Ivy070109 <Ivyaguilar07@gmail.com>
2023-03-30 21:14:49 -0300

    ejercicio 2

---
+++ index.js
@@ -0,0 +1,14 @@
+const estudiante = [
+    {
+        nombre: "Ivana Aguilar",
+        edad: 31,
+        eresDesarrollador: true,
+        nacimiento: new Date(1991, 10, 07),
+        libroFav: {
+            titulo: "Rayuela",
+            autor: "Julio Cortázar",
+            fecha: new Date(1963, 05, 28), 
+            url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Rayuela_JC.png/563px-Rayuela_JC.png"
+        }
+    }
+]
